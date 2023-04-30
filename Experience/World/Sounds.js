3d822c67873c1fc03939168f397b8f1f8c93dcc1
amit dubey <amitdubey6261@gmail.com>
2023-04-30 16:23:09 +0530

    typing-bug-fix

--- Experience/World/Sounds.js
+++ Experience/World/Sounds.js
@@ -4,6 +4,6 @@ export default class{
     constructor(){
         this.experience = new Experience();
         this.items = this.experience.items ; 
-        // this.items.track1.play();
+        this.items.track2.play();
     }
 }
